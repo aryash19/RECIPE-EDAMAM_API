53cd003b52c837333e8d007208fc0bf8bb05ae4c
aryash19 <Aryashchaudhary12341234@gmail.com>
2023-08-02 22:43:07 +0530

    comment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { Routes, Route ,Outlet } from "react-router-dom";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import RecipeDetail from "./pages/RecipeDetail";
 
 
+function Layout(){
+  return(
+    <>
+    <Navbar/>
+    <Outlet/>
+
+    <Footer/>
+    
+    </>
+  )
+}
 function App() {
   return (
     <div className='bg-black'>
-      
-    <Home/>
+      <Routes>
+        <Route path="/" element={<Layout/>} />  
+        <Route index element={<Home/>} />
+        <Route path="recipes/:id" element={<RecipeDetail/>} />
+
+    </Routes>
+
   
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Logo from "../images/logo.png"
+import {HiMenuAlt3} from 'react-icons/hi'
+import {AiOutlineClose} from 'react-icons/ai'
 
 const Navbar = () => {
+  const [open,setOpen] = useState(false)
   return (
-    <div>Navbar</div>
+    <header className='w-full fixed z-10 bg-black'>
+      <nav className='flex w-full py-2 md:py-3 px-4 md:px-20 items-center justify-between'>
+        <a href='/' className='flex items-center justify-center text-white text-lg cursor-pointer' >
+          <img src={Logo} alt="Logo"/>
+          flavor<span>Verse</span>
+
+        </a>
+      </nav>
+    </header>
   )
 }
 
